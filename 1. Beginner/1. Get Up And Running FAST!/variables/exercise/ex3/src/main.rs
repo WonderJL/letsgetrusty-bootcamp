714f3bb20438fdd2f874c549fce46cfcaf0e32cd
WonderJL <wonderjl.tech@gmail.com>
2025-06-02 23:05:58 +0800

    done Variables

---
+++ 1. Beginner/1. Get Up And Running FAST!/variables/exercise/ex3/src/main.rs
@@ -0,0 +1,8 @@
+// Fix this code with shadowing
+
+fn main() {
+    let mut x = "three"; // don't change this line
+    println!("Spell a Number : {}", x);
+    let x = 3; // don't rename this variable
+    println!("Number plus two is : {}", x + 2);
+}
