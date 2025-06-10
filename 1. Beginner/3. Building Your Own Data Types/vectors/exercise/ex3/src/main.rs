eaa58b4c83f19ab69bad2a94a4d189b6a3f64110
WonderJL <wonderjl.tech@gmail.com>
2025-06-10 16:08:37 +0800

    done vectors

---
+++ 1. Beginner/3. Building Your Own Data Types/vectors/exercise/ex3/src/main.rs
@@ -0,0 +1,11 @@
+// Fix the code so that it compiles.
+
+fn main() {
+    let names = vec!["Alice", "Bob", "Cindy"];
+    let index = 2;
+    if let Some(name) = names.get(index) {
+        println!("{name} is present at index {index}");
+    } else {
+        println!("invalid index {index}");
+    }
+}
---
+++ 1. Beginner/3. Building Your Own Data Types/vectors/exercise/ex4/src/main.rs
@@ -0,0 +1,36 @@
+// Fix the code so that it compiles.
+
+struct Student {
+    name: String,
+    marks: u8,
+}
+
+impl Student {
+    fn new(name: &str, marks: u8) -> Self {
+        Self {
+            name: name.to_string(),
+            marks,
+        }
+    }
+}
+
+fn main() {
+    let students = vec![
+        Student::new("Harry", 75),
+        Student::new("Hermoine", 99),
+        Student::new("Ron", 60),
+    ];
+    let mut grades = Vec::new();
+    for student in students.iter() {
+        if student.marks > 80 {
+            grades.push('A');
+        } else if student.marks > 60 {
+            grades.push('B');
+        } else {
+            grades.push('C');
+        }
+    }
+    for i in 0..grades.len() {
+        println!("{} got {}!", students[i].name, grades[i]);
+    }
+}
